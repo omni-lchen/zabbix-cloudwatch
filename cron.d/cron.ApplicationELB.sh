021b22c4ced431990e0b8be8793df842f0be300c
dabest1 <dabest1@users.noreply.github.com>
2018-01-22 19:31:58 -0800

    Added ApplicationELB sample cron job script.

---
+++ cron.d/cron.ApplicationELB.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Description: A script to bulk send ApplicationELB cloudwatch data to zabbix server
+
+PATH=$PATH:/opt/zabbix/cloudwatch
+export PATH
+
+# Load Balancer Name
+LOAD_BALANCER=$1
+# Zabbix Host
+ZABBIX_HOST=$2
+# Zabbix Server
+ZABBIX_SERVER=$3
+# AWS Account
+ACCOUNT=$4
+# AWS Region
+REGION=$5
+# Collecting 5-minute data from cloudwatch
+PERIOD="300"
+# Set start time and end time for collecting cloudwatch data
+# Adding lag, as CloudWatch doesn't send all data if requested too early.
+ENDTIME=$(date -u "+%F %H:%M:00" -d "5 minutes ago")
+STARTTIME=$(date -u "+%F %H:%M:00" -d "10 minutes ago")
+
+# Send cloudwatch data of a table to Zabbix Server
+zabbixCloudWatch.py -z "$ZABBIX_SERVER" -x "$ZABBIX_HOST" -a "$ACCOUNT" -r "$REGION" -s "ApplicationELB" -d "LoadBalancer=$LOAD_BALANCER" -p "$PERIOD" -f "$STARTTIME" -t "$ENDTIME"
