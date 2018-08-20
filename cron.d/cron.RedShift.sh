6025816041b5c2ab1d6c40d8da6cbe7f77fe4843
Труфанов Юрий <38659466+yurij-stranger@users.noreply.github.com>
2018-08-20 19:11:36 +0300

    Update cron.RedShift.sh

--- cron.d/cron.RedShift.sh
+++ cron.d/cron.RedShift.sh
@@ -25,4 +25,4 @@ ENDTIME=$(date -u "+%F %H:%M:00" -d "5 minutes ago")
 STARTTIME=$(date -u "+%F %H:%M:00" -d "10 minutes ago")
 
 # Send cloudwatch data of a table to Zabbix Server
-zabbixCloudWatch.py -z "$ZABBIX_SERVER" -x "$ZABBIX_HOST" -a "$ACCOUNT" -r "$REGION" -s "RedShift" -d "ClusterIdentifier=$CLUSTER_IDENTIFIER" -p "$PERIOD" -f "$STARTTIME" -t "$ENDTIME"
+zabbixCloudWatch.py -z "$ZABBIX_SERVER" -x "$ZABBIX_HOST" -a "$ACCOUNT" -r "$REGION" -s "Redshift" -d "ClusterIdentifier=$CLUSTER_IDENTIFIER" -p "$PERIOD" -f "$STARTTIME" -t "$ENDTIME"
